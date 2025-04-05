7880f26d0adc8fcf2f1715907b07d139f9682853
ezznerf <yakuta04@mail.ru>
2025-04-05 22:20:35 +0300

    migrations types, categories, modify operations

---
+++ database/migrations/2025_04_05_184750_add_fields_to_operations_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('operations', function (Blueprint $table) {
+            $table->timestamp('processed_at')->nullable();
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('item_id')
+                ->references('id')
+                ->on('items')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('operations', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['item_id']);
+
+            $table->dropColumn(['processed_at', 'item_id', 'user_id']);
+
+        });
+    }
+};
